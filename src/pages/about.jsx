9761185a6e2e31e8c42c3a549de47769a2b5290e
aabuff <aabuff@crimson.ua.edu>
2025-09-23 14:51:21 -0500

    Udpated about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,9 +2,13 @@ export default function About() {
   return (
     <div className="max-w-3xl mx-auto mt-10">
       <h1 className="text-3xl font-bold mb-4">About</h1>
-      <p className="text-gray-700">
-        About page
-      </p>
+          <p style={{ marginBottom: '1em' }} className="text-gray-700">
+            Navigate to the "Home" page for a list of churches in the West Alabama area participating in Operation Christmas Child.
+            Use the filters on the left side of the Home page to narrow your search results.
+        </p>
+          <p style={{ marginBottom: '1em' }} className="text-gray-700">
+              The "Profile" page will display your name and the contact information associated with your account.
+        </p>
     </div>
   );
 }
