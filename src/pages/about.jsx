428274117ffd70d5f8daede2b102440e8c90e7f4
aabuff <aabuff@crimson.ua.edu>
2025-09-23 14:56:11 -0500

    "Top" instead of "left"

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ export default function About() {
       <h1 className="text-3xl font-bold mb-4">About</h1>
           <p style={{ marginBottom: '1em' }} className="text-gray-700">
             Navigate to the "Home" page for a list of churches in the West Alabama area participating in Operation Christmas Child.
-            Use the filters on the left side of the Home page to narrow your search results.
+            Use the filters on the top of the Home page to narrow your search results.
         </p>
           <p style={{ marginBottom: '1em' }} className="text-gray-700">
               The "Profile" page will display your name and the contact information associated with your account.
