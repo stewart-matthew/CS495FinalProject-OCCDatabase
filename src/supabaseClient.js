e9dbf8851254dc6988c0feaffcb701336a9194ee
matherdm472 <74468791+matherdm472@users.noreply.github.com>
2025-09-23 15:15:16 -0500

    fixed

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://pwucrztnpxmbpwduvsgd.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3dWNyenRucHhtYnB3ZHV2c2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MTgxMzMsImV4cCI6MjA3MzE5NDEzM30.w-anpAFkRID_aIZtvXsX1O3qk7Pq9Un4QFZr-g_MrDw";
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl) {
   throw new Error("Missing REACT_APP_SUPABASE_URL");
